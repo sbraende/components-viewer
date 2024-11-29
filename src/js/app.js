a3e9563ee55e5261877eb91584f1160957eca2dc
sbraende <sebastian.braende@gmail.com>
2024-11-29 14:33:51 +0100

    implement basic button component

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// SELECT HTML ELEMENTS
+const sidebarComponentButtons = document.querySelectorAll(
+  ".sidebar__components-button"
+);
+
+console.log(sidebarComponentButtons);
+
+// RENDER SELECTED COMPONENTS
