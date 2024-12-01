59f7793dbd08ad41409fbb0e1d106b0718fe1f22
sbraende <sebastian.braende@gmail.com>
2024-12-01 14:59:34 +0100

    add 4 components

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,12 @@ const sidebarButtonContainer = document.querySelector(
   ".sidebar__buttonContainer"
 );
 const componentsSelection = document.querySelectorAll(".components > div");
+const sidebarAddativeCheckbox = document.querySelector(
+  ".sidebar__addativeCheckbox"
+);
+
+// GLOBAL VARIABLES
+const addativeRendering = true;
 
 // RENDER WEBSITE ELEMENTS
 document.addEventListener("DOMContentLoaded", () => renderElements());
@@ -18,16 +24,32 @@ const renderElements = () => {
     sidebarButtonContainer.append(sidebarListItem);
     sidebarListItem.append(sidebarButton);
 
-    // Insert content sidebarButton.
-    sidebarButton.textContent = component.classList.value;
+    // Insert name of component without suffix to textcontent of sidebarButton.
+    sidebarButton.textContent = component.classList.value.split("-")[0];
 
-    // Add class to sidebarButton.
+    // Add class to sidebarButton for styling.
     sidebarButton.classList.add("sidebar__button");
 
     // Eventlistener for sidebarButton.
     sidebarButton.addEventListener("click", () => {
-      // Toggle active eventListener on button.
-      sidebarButton.classList.toggle("sidebar__button--active");
+      if (sidebarAddativeCheckbox.checked) {
+        // Toggle active class on button.
+        sidebarButton.classList.toggle("sidebar__button--active");
+      } else {
+        // Remove remove active class on all buttons
+        const sidebarButtons = document.querySelectorAll(".sidebar__button");
+        sidebarButtons.forEach((sidebarButton) => {
+          if (sidebarButton.classList.contains("sidebar__button--active")) {
+            sidebarButton.classList.remove("sidebar__button--active");
+          }
+        });
+        // Clear all components from components section
+        componentsSelection.forEach((component) => {
+          component.style.display = "none";
+        });
+        // Add active class on current sideButton
+        sidebarButton.classList.add("sidebar__button--active");
+      }
 
       // Only display componentInfo if sidebarButton is active.
       sidebarButton.classList.contains("sidebar__button--active")
