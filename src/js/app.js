579dbfa8356fe2fc0d005041fd8d090522feef16
sbraende <sebastian.braende@gmail.com>
2024-12-01 12:57:11 +0100

    Implement base logic

--- src/js/app.js
+++ src/js/app.js
@@ -4,78 +4,35 @@ const sidebarButtonContainer = document.querySelector(
 );
 const componentsSelection = document.querySelectorAll(".components > div");
 
-// FOR EACH COMPONENT
-componentsSelection.forEach((component, index) => {
-  // CREATE SIDEBAR ELEMENT
-  const sidebarListItem = document.createElement("li");
-  const sidebarButton = document.createElement("button");
+// RENDER WEBSITE ELEMENTS
+document.addEventListener("DOMContentLoaded", () => renderElements());
 
-  // Append sidebar elements.
-  sidebarButtonContainer.append(sidebarListItem);
-  sidebarListItem.append(sidebarButton);
+// RENDER ELEMENTS FUNCTION
+const renderElements = () => {
+  componentsSelection.forEach((component, index) => {
+    // CREATE SIDEBAR ELEMENT
+    const sidebarListItem = document.createElement("li");
+    const sidebarButton = document.createElement("button");
 
-  // Insert content to button
-  sidebarButton.textContent = component.classList.value;
+    // Append sidebar elements to parents.
+    sidebarButtonContainer.append(sidebarListItem);
+    sidebarListItem.append(sidebarButton);
 
-  // Add class to elements.
-  sidebarButton.classList.add("sidebar__button");
+    // Insert content sidebarButton.
+    sidebarButton.textContent = component.classList.value;
 
-  // Store content of element
-  originalTextContentComponent = component.textContent;
+    // Add class to sidebarButton.
+    sidebarButton.classList.add("sidebar__button");
 
-  // Toggle active eventListener on button.
-  sidebarButton.addEventListener("click", () => {
-    sidebarButton.classList.toggle("sidebar__button--active");
+    // Eventlistener for sidebarButton.
+    sidebarButton.addEventListener("click", () => {
+      // Toggle active eventListener on button.
+      sidebarButton.classList.toggle("sidebar__button--active");
 
-    // If current button is acitve.
-    if (sidebarButton.classList.contains("sidebar__button--active")) {
-      component.textContent = originalTextContentComponent;
-    } else {
-      component.textContent = "";
-      console.log(component);
-    }
+      // Only display componentInfo if sidebarButton is active.
+      sidebarButton.classList.contains("sidebar__button--active")
+        ? (component.style.display = "block")
+        : (component.style.display = "none");
+    });
   });
-});
-
-/*
- *
- * OLD STUFF
- *
- */
-
-// // SELECT HTML ELEMENTS
-// const sidebarButtons = document.querySelectorAll(".sidebar__button");
-// const componentButton = document.querySelector(".component__button");
-// const componentSelect = document.querySelector(".component__select");
-
-// // RENDER ON PAGE OPEN
-// document.addEventListener("DOMContentLoaded", () => renderComponents());
-
-// // RENDER SELECTED COMPONENTS
-// const renderComponents = () => {
-//   // ComponentsSections that are active should render on page.
-//   sidebarButtons.forEach((button, index) => {
-//     // Get reference to componentSection based on sidebarButton clicked.
-//     const componentSection = document.querySelector(
-//       ".component__" + button.value
-//     );
-
-//     // If sidebarButton is NOT active, hide the content of the componentSection.
-//     if (!button.classList.contains("sidebar__button--active")) {
-//       componentSection.textContent = "";
-//     }
-//   });
-// };
-
-// sidebarButtons.forEach((button, index) => {
-//   button.addEventListener("click", () => {
-//     console.log("button clicked");
-//     // Toggle active class on button.
-//     button.classList.toggle("sidebar__button--active");
-//     renderComponents();
-//     // TODO: I'm deleting all the contents inside the section sidebar.
-//     // Probably need to make a quaryAll for "components" in the begninning, storing init information.
-//     // Then we can filter based on that.
-//     // Could we use filter in general? Maybe less distrucive than removing information...
-//   });
-// });
+};
